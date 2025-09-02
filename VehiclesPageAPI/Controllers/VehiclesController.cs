35f0b33ed2d0fac3fc0d71777718dff3a67aae1c
branquelao <henriquetozzonarciso@gmail.com>
2025-09-02 11:56:31 -0300

    Added front end Vehicles tab

--- VehiclesPageAPI/Controllers/VehiclesController.cs
+++ VehiclesPageAPI/Controllers/VehiclesController.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using VehiclesPageAPI.Services;
 
 namespace VehiclesPageAPI.Controllers
 {
     public class VehiclesController : Controller
     {
+        private readonly ApplicationDbContext context;
+
+        public VehiclesController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            var vehicles = context.Vehicles.ToList();
+            return View(vehicles);
         }
     }
 }
