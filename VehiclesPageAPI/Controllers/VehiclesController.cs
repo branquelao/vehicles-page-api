a7435eafa034344988f69a5d1bad77c149533aed
branquelao <henriquetozzonarciso@gmail.com>
2025-09-05 11:10:07 -0300

    Finalized Create Vehicle method

--- VehiclesPageAPI/Controllers/VehiclesController.cs
+++ VehiclesPageAPI/Controllers/VehiclesController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+using VehiclesPageAPI.Models;
 using VehiclesPageAPI.Services;
 
 namespace VehiclesPageAPI.Controllers
@@ -6,15 +8,61 @@ namespace VehiclesPageAPI.Controllers
     public class VehiclesController : Controller
     {
         private readonly ApplicationDbContext context;
+        private readonly IWebHostEnvironment environment;
 
-        public VehiclesController(ApplicationDbContext context)
+        public VehiclesController(ApplicationDbContext context, IWebHostEnvironment environment)
         {
             this.context = context;
+            this.environment = environment;
         }
         public IActionResult Index()
         {
-            var vehicles = context.Vehicles.ToList();
+            var vehicles = context.Vehicles.OrderByDescending(p => p.Id).ToList();
             return View(vehicles);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(VehicleDto vehicleDto)
+        {
+            if(vehicleDto.ImageFile == null)
+            {
+                ModelState.AddModelError("ImageFile", "Please select an image file.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(vehicleDto);
+            }
+
+            string newFileName = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+            newFileName += Path.GetExtension(vehicleDto.ImageFile!.FileName);
+
+            string imageFullPath = environment.WebRootPath + "/vehicles/" + newFileName;
+            using (var stream = System.IO.File.Create(imageFullPath))
+            {
+                vehicleDto.ImageFile.CopyTo(stream);
+            }
+
+            Vehicle vehicle = new Vehicle
+            {
+                Make = vehicleDto.Make,
+                Model = vehicleDto.Model,
+                Category = vehicleDto.Category,
+                Price = vehicleDto.Price,
+                Description = vehicleDto.Description,
+                ImageFileName = newFileName,
+                CreatedAt = DateTime.Now
+            };
+
+            context.Vehicles.Add(vehicle);
+            context.SaveChanges();
+
+            return RedirectToAction("Index", "Vehicles");
         }
     }
 }
---
+++ VehiclesPageAPI/Models/VehicleDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VehiclesPageAPI.Models
+{
+    public class VehicleDto
+    {
+        [Required, MaxLength(15)]
+        public string Make { get; set; } = "";
+
+        [Required, MaxLength(50)]
+        public string Model { get; set; } = "";
+
+        [Required, MaxLength(15)]
+        public string Category { get; set; } = "";
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public string Description { get; set; } = "";
+        public IFormFile? ImageFile { get; set; }
+    }
+}
